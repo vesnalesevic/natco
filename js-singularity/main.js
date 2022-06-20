88f2409156cc1079ca591efdea7df89dacea712b
vesnalesevic <vesna.lesevic85@gmail.com>
2022-06-20 15:23:55 +0200

    new files

---
+++ js-singularity/main.js
@@ -0,0 +1,7 @@
+// change navbar styles on scroll
+
+
+var hs = document.querySelectorAll("h3");
+for (i = 0; i < hs.length; ++i) {
+    hs[i].innerHTML = hs[i].innerHTML.replace(/&nbsp;/g, " ");
+}
